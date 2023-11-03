73e61a3407932c198cd77e15e8ee2bfb531d8da5
cin-moriwaki <moriwaki-h@cin-group.com>
2023-11-03 09:42:26 +0900

    変数と可変性

--- myapp/src/main.rs
+++ myapp/src/main.rs
@@ -1,3 +1,18 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+// 変数と可変性
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {}", x);     // xの値は{}です
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
